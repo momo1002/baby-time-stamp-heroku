471ddd69ecb30f27c9df561def9ef4f689f0584c
momo <momoko.nakada1002@gmail.com>
2021-07-28 03:56:27 -0700

    Improve

--- index.js
+++ index.js
@@ -1,85 +1,71 @@
-// -----------------------------------------------------------------------------
-// モジュールのインポート
-const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+const https = require("https")
+const express = require("express")
+const app = express()
 
-// -----------------------------------------------------------------------------
-// パラメータ設定
-const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
-};
+const PORT = process.env.PORT || 3000
+const TOKEN = process.env.LINE_ACCESS_TOKEN
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-server.listen(process.env.PORT || 3000);
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
 
+app.get("/", (req, res) => {
+    res.sendStatus(200)
+})
+  
+app.post("/bot/webhook", function(req, res) {
+    res.send("HTTP POST request sent to the webhook URL!")
+    // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
+    if (req.body.events[0].type === "message") {
+      // 文字列化したメッセージデータ
+      const dataString = JSON.stringify({
+        replyToken: req.body.events[0].replyToken,
+        messages: [
+          {
+            "type": "text",
+            "text": "Hello, user"
+          },
+          {
+            "type": "text",
+            "text": "May I help you?"
+          }
+        ]
+      })
+  
+      // リクエストヘッダー
+      const headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + TOKEN
+      }
+  
+      // リクエストに渡すオプション
+      const webhookOptions = {
+        "hostname": "api.line.me",
+        "path": "/v2/bot/message/reply",
+        "method": "POST",
+        "headers": headers,
+        "body": dataString
+      }
+  
+      // リクエストの定義
+      const request = https.request(webhookOptions, (res) => {
+        res.on("data", (d) => {
+          process.stdout.write(d)
+        })
+      })
+  
+      // エラーをハンドル
+      request.on("error", (err) => {
+        console.error(err)
+      })
+  
+      // データを送信
+      request.write(dataString)
+      request.end()
+    }
+})
 
-// -----------------------------------------------------------------------------
-// APIコールのためのクライアントインスタンスを作成
-const bot = new line.Client(line_config);
-
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
-    // 先行してLINE側にステータスコード200でレスポンスする。
-    res.sendStatus(200);
-
-    // すべてのイベント処理のプロミスを格納する配列。
-    let events_processed = [];
-
-    // イベントオブジェクトを順次処理。
-    req.body.events.forEach((event) => {
-        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "これはこれは"
-                }));
-            }
-            if (event.message.text == "こんばんは"){
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "うざ"
-                }));
-            }
-
-            const msgOppai = [
-                {
-                  'type':'text',
-                  'text': '「おちまい」タップで時間をキロクできるよ',
-                  "quickReply": {
-                    "items": [
-                      {
-                        "type": "action",
-                        "action": {
-                          "type": "message",
-                          "label": "🕐おっぱいタイムおちまい",
-                          "text": "おっぱいタイムおちまい"
-                        }
-                      }
-                    ]
-                  }
-                }];
-            let msg;
-            if (event.message.text == "おっぱい"){
-                msg = msgOppai;
-                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    message: msg
-                }));
-            }
-        }
-    });
-
-    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
-    Promise.all(events_processed).then(
-        (response) => {
-            console.log(`${response.length} event(s) processed.`);
-        }
-    );
-});
+app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`)
+})
